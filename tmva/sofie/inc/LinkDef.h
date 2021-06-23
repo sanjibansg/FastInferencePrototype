c0e0f431fc128dfe8caaacbe25c9e0e6efa85175
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2021-06-23 23:36:19 +0530

    SOFIE Restructured

---
+++ tmva/sofie/inc/LinkDef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link off all namespaces;
+
+#pragma link C++ nestedclass;
+
+
+// the classifiers
+#pragma link C++ class TMVA::Experimental::SOFIE::RModel+;
+
+#endif
